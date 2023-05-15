b51af523a7252e833f27c33e87962ef8b1e7ac15
joshjt96 <jtdev84@gmail.com>
2023-05-15 17:22:01 +0100

    Add examples

--- examples.py
+++ examples.py
@@ -34,3 +34,75 @@ customer_data[1].remove(False)
 new_customers = [["Amit", "Large", True], ["Karim", "X-Large", False]]
 customer_data_final = customer_data + new_customers
 print(customer_data_final)
+
+# Consectutive lists: range
+
+number_list = range(9)
+print(list(number_list))
+
+zero_to_seven = range(8)
+print(list(zero_to_seven))
+
+# Slicing lists
+
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+beginning = suitcase[0:2]
+
+print(beginning)
+
+middle = suitcase[2:4]
+print(middle)
+
+# Slicing 2:
+
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+# Your code below: 
+
+last_two_elements = suitcase[-2:]
+print(last_two_elements)
+
+slice_off_last_three = suitcase[:-3]
+print(slice_off_last_three)
+
+# .count method:
+
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+# Your code below: 
+jake_votes = votes.count("Jake")
+print(jake_votes)
+# This prints the amount of times Jake appears in the list.
+
+# Sorting lists with reverse
+
+# Checkpoint 1 & 2
+addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
+
+
+# Checkpoint 3
+names = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
+names.sort()
+
+
+# Checkpoint 4 & 5
+cities = ["London", "Paris", "Rome", "Los Angeles", "New York"]
+sorted_cities = cities.sort(reverse=True)
+
+addresses.sort()
+print(addresses)
+
+print(sorted_cities)
+
+# Sorting lists with sorted
+
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+games_sorted = sorted(games)
+print(games)
+print(games_sorted)
+
+# Remove 5th element in list with pop:
+
+removed_item = list.pop(4)

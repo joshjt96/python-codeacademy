e7155de1ef3100f3ba8bfae0acc5a92165f51849
joshjt96 <jtdev84@gmail.com>
2023-05-15 11:19:54 +0100

    Finish shipping.py

--- shipping.py
+++ shipping.py
@@ -1,15 +1,39 @@
-weight = 8.4
+weight = 4.8
 
 # Ground Shipping
 
 ground_flat_charge = 20
+premium_shipping = 125
+ground_shipping_text = "Ground Shipping Cost: "
+
+print("Ground Shipping Premium: $", + premium_shipping)
 
 if weight <= 2:
   ground_shipping = weight * 1.5 + ground_flat_charge
-  print(ground_shipping)
+  print(ground_shipping_text, + ground_shipping)
 elif weight > 2 and weight <= 6:
-  print("$3.00")
+    ground_shipping = weight * 3 + ground_flat_charge
+    print(ground_shipping_text, + ground_shipping)
 elif weight > 6 and weight <= 10:
-  print("$4.00")
+    ground_shipping = weight * 4 + ground_flat_charge
+    print(ground_shipping_text, + ground_shipping)
 elif weight > 10:
-  print("$4.75")
+    ground_shipping = weight * 4.75 + ground_flat_charge
+    print(ground_shipping_text, + ground_shipping)
+
+# Drone Shipping
+
+drone_shipping_text = "Drone Shipping Cost: "
+
+if weight <= 2:
+  drone_shipping = weight * 4.50
+  print(drone_shipping_text, + drone_shipping)
+elif weight > 2 and weight <= 6:
+    drone_shipping = weight * 9
+    print(drone_shipping_text, + drone_shipping)
+elif weight > 6 and weight <= 10:
+    drone_shipping = weight * 12
+    print(drone_shipping_text, + drone_shipping)
+elif weight > 10:
+    drone_shipping = weight * 14.25
+    print(drone_shipping_text, + drone_shipping)
